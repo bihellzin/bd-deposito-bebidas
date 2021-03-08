88b38273a02b4b6d99c798cfc83f4526cd133a58
bielzin <oliva.gabriel@outlook.com>
2021-03-08 07:45:00 -0300

    Adiciona model, controller e view de Funcionario, mas ainda incompleto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "funcionarios#index"
+
+  get "/funcionarios", to: "funcionarios#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
