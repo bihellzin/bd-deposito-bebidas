cf01c2c3902157202f73810c44531986dafc42c0
bielzin <oliva.gabriel@outlook.com>
2021-03-11 09:27:17 -0300

    Adiciona Fornecedor

---
+++ db/migrate/20210311122621_create_fornecedors.rb
@@ -0,0 +1,10 @@
+class CreateFornecedors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :fornecedors do |t|
+      t.integer :cnpj
+      t.string :nome
+
+      t.timestamps
+    end
+  end
+end
